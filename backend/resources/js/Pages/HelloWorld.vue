2279c01d10f69a8ab25481710ab0931c7e07fa33
ship <shipwebdotjp@gmail.com>
2023-08-12 16:12:10 +0900

    #2 Hello World

---
+++ backend/resources/js/Pages/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
+
+<template>
+    <div>
+        Hello world!
+        <Link href="/HelloWorld" class="text-sm text-blue-700 underline">
+                HelloWorld
+        </Link>
+    </div>
+</template>
--- backend/vite.config.js
+++ backend/vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
     ],
+    //この下の部分を追加
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
