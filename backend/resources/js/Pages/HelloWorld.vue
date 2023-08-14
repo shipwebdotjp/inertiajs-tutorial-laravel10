32ee6c0122e497c662a132df508caab9ade11499
ship <shipwebdotjp@gmail.com>
2023-08-14 21:43:07 +0900

    #3 Page Layout

--- backend/resources/js/Pages/HelloWorld.vue
+++ backend/resources/js/Pages/HelloWorld.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
+import Layout from '@/Pages/Layout.vue'
 </script>
 
 <template>
-    <div>
+    <Layout title="HelloWorld">
         Hello world!
         <Link href="/HelloWorld" class="text-sm text-blue-700 underline">
                 HelloWorld
         </Link>
-    </div>
+    </Layout>
 </template>
